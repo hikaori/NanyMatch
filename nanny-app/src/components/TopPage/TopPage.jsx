ff35ad960814cff002384a035211c5de78149296
hikaori <kaori55194@gmail.com>
2018-10-21 01:19:38 -0700

    add nannypart in toppage

--- nanny-app/src/components/TopPage/TopPage.jsx
+++ nanny-app/src/components/TopPage/TopPage.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 import Container from '../Container/Container.jsx';
+import NannyItem from './NannyItem/NannyItem.jsx';
+import { nannyDetails } from '../../constants/nannyDetails.js';
+import './TopPage.scss'
 export default class TopPage extends Component {
   render() {
     return (
       <Container>
         <div className="TopPage">
-        === top =====
+          <img src="" alt="" />
+          <div className="Top">
+            <div>We are Nanny From Japan. We bring you the relief you need, when you need it.</div>
+            <button className="">Let's Go Find Your Nanny</button>
+          </div>
+          <h2>Our Featured Nannies</h2>
+          <div className="NannyItemContainer">
+            {nannyDetails.map((nannyId)=>(
+              <NannyItem 
+                key={nannyId.id}
+                name={nannyId.name}
+                imageUrl={nannyId.imageUrl}
+                workExperience={nannyId.workExperience}
+                availability={nannyId.availability}
+                wage={nannyId.wage}
+                comments={nannyId.comments}
+                live={nannyId.live}
+              />
+            ))}
+          </div>
         </div>
       </Container>
     )
---
+++ nanny-app/src/constants/nannyDetails.js
@@ -0,0 +1,32 @@
+export const nannyDetails = [
+  {
+    id:0,
+    name: 'nanny0',
+    imageUrl: 'nannyImage',
+    workExperience :'Education',
+    availability: 'Live-Out',
+    wage: 13,
+    comments:'Hi there, my name is Erika. I’m an easy going person, I love work with kids, I like coooking and enjoy all the outdoor or indoor activities.',
+    live:'BC'
+  },
+  {
+    id:1,
+    name: 'nanny1',
+    imageUrl: 'nannyImage',
+    workExperience :'Education',
+    availability: 'Live-Out',
+    wage: 13,
+    comments:'Hi there, my name is Erika. I’m an easy going person, I love work with kids, I like coooking and enjoy all the outdoor or indoor activities.',
+    live:'BC'
+  },
+  {
+    id:2,
+    name: 'nanny2',
+    imageUrl: 'nannyImage',
+    workExperience :'Education',
+    availability: 'Live-Out',
+    wage: 13,
+    comments:'Hi there, my name is Erika. I’m an easy going person, I love work with kids, I like coooking and enjoy all the outdoor or indoor activities.',
+    live:'BC'
+  },
+]
