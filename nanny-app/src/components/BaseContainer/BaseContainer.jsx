6a82888e006bc38c4502ad953fcfd3db320ee2cd
hikaori <kaori55194@gmail.com>
2018-10-21 17:03:39 -0700

    made link for mamberTopContainer

--- nanny-app/src/components/BaseContainer/BaseContainer.jsx
+++ nanny-app/src/components/BaseContainer/BaseContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { NonMemberTopContainer } from '../../components';
+import { MemberTopContainer } from '../../components';
 import About from '../About/About';
 import NotFound from '../NotFound/NotFound';
 import './BaseContainer.scss';
@@ -12,6 +13,7 @@ export default class Container extends Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={NonMemberTopContainer} />
+            <Route exact path="/member" component={MemberTopContainer} />
             <Route path="/about" component={About} />
             <Route path="/404" component={NotFound} />
             <Redirect from="*" to="404" />
--- nanny-app/src/components/index.js
+++ nanny-app/src/components/index.js
@@ -1,5 +1,5 @@
-import NonMemberTopContainer from './NonMemberTopContainer/NonMemberTopContainer'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-export { NonMemberTopContainer , Header ,Footer };
-
+import NonMemberTopContainer from './NonMemberTopContainer/NonMemberTopContainer';
+import MemberTopContainer from './NonMemberTopContainer/NonMemberTopContainer';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+export { NonMemberTopContainer, MemberTopContainer, Header, Footer };
