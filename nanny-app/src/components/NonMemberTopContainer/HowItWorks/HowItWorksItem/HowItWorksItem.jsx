978ed10eba02d70932d0ee7edbe5d72c0c1a2542
hikaori <kaori55194@gmail.com>
2018-10-27 15:20:58 -0700

    add style for how it works

--- nanny-app/src/components/NonMemberTopContainer/HowItWorks/HowItWorksItem/HowItWorksItem.jsx
+++ nanny-app/src/components/NonMemberTopContainer/HowItWorks/HowItWorksItem/HowItWorksItem.jsx
@@ -1,13 +1,27 @@
 import React, { Component, Fragment } from 'react';
+import { Button } from '../../../index';
+import './HowItWorksItem.scss';
 export default class HowItWorksItem extends Component {
   render() {
     const { title, imageUrl, buttonText } = this.props;
     return (
-      <Fragment>
-        <h3>{title}</h3>
-        <img src={imageUrl} alt={title} />
-        {buttonText && <button>{buttonText}</button>}
-      </Fragment>
+      <div className="HowItWorksItem">
+        <span>
+          <span className="HowItWorksItem-Num" />
+        </span>
+        <section>
+          <h3>{title}</h3>
+          <img src={imageUrl} alt={title} />
+          {buttonText && (
+            <Button
+              hasBorder={false}
+              isShowly={false}
+              text={buttonText}
+              toLink={'/about'}
+            />
+          )}
+        </section>
+      </div>
     );
   }
 }
