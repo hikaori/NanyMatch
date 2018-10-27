655f06e596b0a055fb2e59d3896e73ec88c6408e
hikaori <kaori55194@gmail.com>
2018-10-27 15:27:16 -0700

    add image for how it works

--- nanny-app/src/constants/howItWorksItems.js
+++ nanny-app/src/constants/howItWorksItems.js
@@ -1,22 +1,27 @@
+import HowItWorksimg1 from '../assets/images/home_howItAllworks_lookForYourNanny.jpg';
+import HowItWorksimg2 from '../assets/images/home_howItAllworks_InterviewYourNanny.jpg';
+import HowItWorksimg3 from '../assets/images/home_howItAllworks_applyForNannysWorkPermit.jpg';
+import HowItWorksimg4 from '../assets/images/home_howItAllworks_nannyStartsToWork.jpg';
+
 export const howItWorksItems = [
   {
     title: 'Look For Your Nanny',
-    imageUrl: '',
+    imageUrl: HowItWorksimg1,
     buttonText: 'Go Look For Your Nanny'
   },
   {
     title: 'Interview Your Nanny',
-    imageUrl: '',
+    imageUrl: HowItWorksimg2,
     buttonText: null
   },
   {
     title: 'Apply For Nanny’s Work Permit',
-    imageUrl: '',
+    imageUrl: HowItWorksimg3,
     buttonText: 'Learn About Work Elibility'
   },
   {
     title: 'Nanny Starts To Work',
-    imageUrl: '',
+    imageUrl: HowItWorksimg4,
     buttonText: 'Learn About Work Elibility'
   }
 ];
