2e33dc222607680048560c4cb9702fc854899332
hikaori <kaori55194@gmail.com>
2018-10-29 12:48:17 -0700

    add style for review area

--- nanny-app/src/components/NonMemberTopContainer/NFJReviewContainer/NFJReviewContainer.jsx
+++ nanny-app/src/components/NonMemberTopContainer/NFJReviewContainer/NFJReviewContainer.jsx
@@ -1,18 +1,19 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { SliderContainer } from '../../';
+import './NFJReviewContainer.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 export default class TopPage extends Component {
   render() {
     return (
-      <Fragment>
+      <div className="NFJReviewContainer">
         <h2>Voices We’ve Received</h2>
         <SliderContainer
           category={'review'}
           slidesToShowNum={1}
           slidesToScrollNum={1}
         />
-      </Fragment>
+      </div>
     );
   }
 }
